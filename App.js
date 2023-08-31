48fc6f8a1bd88abfefc1a936e29781458a938389
Javier Lopéz <jlopezhdez94@gmail.com>
2023-08-31 22:41:17 +0100

    Start

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  TouchableOpacity,
+} from "react-native";
+import Header from "./source/components/Header";
+import { useState } from "react";
+import Timer from "./source/components/Timer";
+
+const colors = ["white", "green", "red"];
 
 export default function App() {
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(25 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [currentTime, setCurrentTime] = useState("Twenty" || "Then" || " Five");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView
+      style={[styles.container, { backgroundColor: colors[currentTime] }]}
+    >
+      <View style={{ paddingTop: Platform.OS === "android" && 30 }}>
+        <Text style={styles.app_name}> Timer App</Text>
+        <Header
+          currentTime={currentTime}
+          setCurrentTime={setCurrentTime}
+          setTime={setTime}
+        />
+        <Timer time={time} />
+        <Text style={{ color: "black", fontWeight: "bold" }}>{time}</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={{ color: "white", fontWeight: "bold" }}>Start</Text>
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  app_name: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "blue",
+    marginLeft: 100,
+    paddingTop: 20,
+  },
+  button: {
+    backgroundColor: "#333333",
+    padding: 12,
+    marginTop: 15,
+    borderRadius: 15,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "35%",
+    marginLeft: 135,
   },
 });
---
+++ source/components/Header.jsx
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const options = ["Twenty", "Then", " Five"];
+
+export default function Header({ currentTime, setCurrentTime, setTime }) {
+  function handlepress(index) {
+    const newTime = index === 0 ? 20 : index === 1 ? 10 : 5;
+    setCurrentTime(index);
+    setTime(newTime * 60);
+  }
+
+  return (
+    <>
+      <View style={styles.view_principal}>
+        {options.map((item, index) => (
+          <TouchableOpacity
+            key={index}
+            onPress={() => handlepress(index)}
+            style={[
+              styles.itemStyle,
+              currentTime !== index && { borderColor: "transparent" },
+            ]}
+          >
+            <Text>{item}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  view_principal: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 30,
+  },
+  itemStyle: {
+    width: "33%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 3,
+    padding: 7,
+    borderRadius: 20,
+  },
+});
